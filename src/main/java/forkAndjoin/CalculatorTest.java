3e63b1682b15326d95fc2678296f5dd7dd236993
caijianfu <buildhappy512@163.com>
2015-07-17 11:00:09 +0800

    initialize project

---
+++ src/main/java/forkAndjoin/CalculatorTest.java
@@ -0,0 +1,26 @@
+package forkAndjoin;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Author: zhongxin
+ * Created at: 2010-4-14 13:40:58
+ */
+public class CalculatorTest { 
+
+    //@Test
+    public void run() throws Exception{
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        Future<Integer> result = forkJoinPool.submit(new Calculator(0, 10000));
+        assertEquals(new Integer(49995000), result.get());
+    }
+
+
+}
+
+
+
